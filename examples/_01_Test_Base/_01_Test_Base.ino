a2ce5622644f106321b104ecf161f03417b021ec
Rafael Medina <rafaelmedina789@gmail.com>
2017-08-03 10:20:19 -0600

    Files with new structure

--- examples/_01_Test_Base/_01_Test_Base.ino
+++ examples/_01_Test_Base/_01_Test_Base.ino
@@ -10,10 +10,14 @@
 #include <RF24.h>
 #include <SPI.h>
 
+#define NODE_ID 1
+#define CHANNEL 65
+
 // nRF24L01/+ must be connected to digital pins 9, 10, 11, 12, 13
 // Out of the above 9 and 10 can be reconfigured if needed
 // Digital pins 11, 12 and 13 are for SPI and cannot be changed
 Mesh24 mesh24(9, 10);
+Mesh24Timer sendTimer(3000);
 
 void setup() {
   Serial.begin(57600);
@@ -22,11 +26,11 @@ void setup() {
   // It must be between 0 and 127
   // Uncomment the follow line to change the default which is 75
   // Use value less then 80 in US to comply with FCC (at least that is my understanding)
-//  mesh24.setChannel(60);
+  mesh24.setChannel(CHANNEL);
 
   // Must be unique for all nodes
   // Heartbeats from all nodes are sent to address 1
-  mesh24.setAddr(1);
+  mesh24.setAddr(NODE_ID);
   
   // Initialize radio
   mesh24.begin();
@@ -34,17 +38,40 @@ void setup() {
 
 void loop() {
   Mesh24Message message;
-
-  // Read new message, if any
   if (mesh24.read(message)) {
     // Print message content in hex
     Serial.print(millis());
     Serial.print(" msg=");
     Mesh24Utils.printAsHex(Serial, message.getBuffer(), MESH24_MESSAGE_SIZE);
     Serial.println();
+  }
+  
+  if (sendTimer.isDue()) {
+    Mesh24Message payload(NODE_ID, 2, MESH24_MSG_HEARTBEAT);   
+    unsigned long msg = 420;
+    payload.writePayload(msg);    
+    mesh24.write(payload,true);
+    Serial.print(millis());
+    Serial.print(" payLoad=");
+    Mesh24Utils.printAsHex(Serial, payload.getBuffer(), MESH24_MESSAGE_SIZE);
+    Serial.println();
     
-    // If heartbeat
-    if (message.getType() == MESH24_MSG_HEARTBEAT) {
+  }
+/*
+  // Read new message, if any
+  if (mesh24.read(message)) {
+    // Print message content in hex
+    Serial.print(millis());
+    Serial.print(" msg=");
+    unsigned msg;
+    message.readPayload(msg);
+    Serial.print(msg);
+    Serial.println();
+  }
+  */
+  
+
+   /* if (message.getType() == MESH24_MSG_HEARTBEAT) {
       // Parse parameters (uptime, temperature, and voltage)
       unsigned long uptime;
       unsigned temperature;
@@ -74,9 +101,7 @@ void loop() {
       Serial.print(uptime);
       Serial.println("s");
     }
-  }
-  
-  // Give the mcu a little break
-  delay(1);
+    else
+    {*/
 }
 
